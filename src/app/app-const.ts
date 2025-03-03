390bc3cc3872a392e896200c369be4905a987beb
Prayag_s29 <prayag_sawant@saiservice.com>
2025-03-03 13:52:02 +0530

    reports API calling changes 03-03

--- src/app/app-const.ts
+++ src/app/app-const.ts
@@ -1,5 +1,5 @@
 export class AppConst {
     public static get ServerUrl(): string { return "http://localhost:8080"; }
-    // public static get ServerUrl(): string { return "http://saiitportal.com"; }
+    
 
 }
