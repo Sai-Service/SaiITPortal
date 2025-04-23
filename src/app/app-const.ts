65fa7a88555592f8b64b9dd3f5bcb73ee320d65c
Prayag_s29 <prayag_sawant@saiservice.com>
2025-04-23 17:54:32 +0530

    changes by prayag 23-04 add testing url

--- src/app/app-const.ts
+++ src/app/app-const.ts
@@ -1,6 +1,7 @@
 export class AppConst {
-    public static get ServerUrl(): string { return "http://localhost:8080"; }
+    // public static get ServerUrl(): string { return "http://localhost:8080"; }
     //  public static get ServerUrl(): string { return "http://saiitportal.com/ITPortal"; }
+     public static get ServerUrl(): string { return "http://saiitportaltest.com/ITPortal"; }
     
 
 }
